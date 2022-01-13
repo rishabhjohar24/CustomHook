3d9513fbfd94889175fd0407cb2f85ebc98084f7
Rishabh_J <62639342+rishabhjohar24@users.noreply.github.com>
2022-01-13 16:41:13 +0530

    First

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,33 @@
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+  const createdTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+  const enterTaskHandler = async (taskText) => {
+    const requestConfig = {
+      url: "https://react-basics-83130-default-rtdb.firebaseio.com/tasks.json",
+      method: "POST",
+      body: { text: taskText },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    sendTaskRequest(requestConfig, createdTask.bind(null, taskText));
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
